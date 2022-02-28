b24f6b06f5b05119d4e3124cf94fcac94ea0303b
gabrielavl298 <gaby.12leos@gmail.com>
2022-02-28 00:05:05 -0600

    images update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FirebaseDataService, Producto } from 'src/app/firebase-data.service';
 import { Observable } from 'rxjs';
 import Swal from 'sweetalert2';
 import {Router} from '@angular/router';
+import { ConditionalExpr } from '@angular/compiler';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -17,12 +20,15 @@ export class HomeComponent implements OnInit {
   public emailauth;
   public observableProducto: Observable<Producto[]>;
    filtro: Producto[] ;
+  public uploadPercent: Observable<number>;
+  public urlImage: Observable<string>;
   constructor(
     private router:Router,
     public auth: AngularFireAuth,
     firestore: AngularFirestore,
     public formBuilder: FormBuilder,
     public firebaseService: FirebaseDataService,
+    private storage: AngularFireStorage
   ) {
     this.productoForm = formBuilder.group({
       nombre: ['', [Validators.required]],
@@ -30,10 +36,13 @@ export class HomeComponent implements OnInit {
       descripcion: ['', [Validators.required]],
       precio: ['', [Validators.required]],
       titulo: ['', [Validators.required]],
-      image:['', [Validators.required]]
+      imageURL:['', [Validators.required]]
     })
     this.filtro= [];
     this.emailauth='';
+    this.uploadPercent = new Observable<number>();
+    this.urlImage = new Observable<string>();
+
     this.observableProducto = this.firebaseService.GetProducto();
     this.observableProducto.forEach(element=> {
       for(var i = 0; i<element.length; i++) {
@@ -58,16 +67,34 @@ export class HomeComponent implements OnInit {
     
   }
 
-  async OnSubmit() {
+  
+  public onUpload(e: any){
+    console.log("Entro aqui", e)
 
+    const id = Math.random().toString(36).substring(2);
+    const file = e.target.files[0];
+    const filePath = this.emailauth + `/img_${id}`;
+    const ref = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+
+    //this.uploadPercent = task.percentageChanges();
+
+    task.snapshotChanges().pipe(finalize(() => this.urlImage = ref.getDownloadURL())).subscribe();
+
+  }
+
+  async OnSubmit() {
+    let url = (<HTMLInputElement>document.getElementById("imageURL")).value;
+    console.log("URL image: ", url);
     const tempProducto: Producto = {
       nombre: this.productoForm.value.nombre,
       id_empresa: this.emailauth,
       descripcion: this.productoForm.value.descripcion,
       precio: Number(this.productoForm.value.precio),
       titulo:this.productoForm.value.titulo,
-      image:this.productoForm.value.titulo
+      image: (<HTMLInputElement>document.getElementById("imageURL")).value
     }
+    console.log("Producto model: ", tempProducto);
     await this.firebaseService.AddProducto(tempProducto);
     Swal.fire('Producto Agregado!');
     this.clearForm();
@@ -82,7 +109,8 @@ export class HomeComponent implements OnInit {
       descripcion: '',
       precio: '',
       titulo:'',
-      image:''
+      image:'',
+      imageURL: ''
     })
   }
 
